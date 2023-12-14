60744c1eb9fc0fd84f20e3998bcc40888bf40c59
Emil Elsetronning <emilandreelsetronning@gmail.com>
2023-12-14 17:23:55 +0100

    trying something else

--- 2023/day13/solver/part_1.py
+++ 2023/day13/solver/part_1.py
@@ -1,4 +1,5 @@
 from solver import utils
+import numpy as np
 
 
 def solve(input_file: str):
@@ -6,23 +7,43 @@ def solve(input_file: str):
     patterns = [x.split("\n") for x in lines.split("\n\n")]
     print()
 
+    #for pattern in patterns:
+    #    print()
+    #    for line in pattern:
+    #        print(line)
+    
+
+    print("\nSolve")
+    sums = []
+
+    def find_mirror(pattern, multiplier):
+        pattern_len = len(pattern)
+        for i in range(len(pattern)-1):
+            if pattern[i] == pattern[i+1]:
+                top = pattern[:i+1]
+                bottom = pattern[i+1:]
+
+                len_top = len(top)
+                len_bottom = len(bottom)
+                if len_top != len_bottom:
+                    if len_top > len_bottom:
+                        top = top[len_top-len_bottom:]
+                    if len_top < len_bottom:
+                        bottom = bottom[:-(len_bottom-len_top)]
+
+                if top == list(reversed(bottom)):
+                    return multiplier*(i+1)
+        return 0
+
     for pattern in patterns:
         print()
+        sums.append(find_mirror(pattern, 100))
+        rotated = list(["".join(x) for x in zip(*pattern[::-1])])
+        sums.append(find_mirror(rotated, 1))
+
         for line in pattern:
             print(line)
-    
-    print("\nSolve")
-    for pattern in patterns:
-        print()
-        # find vertical mirrors
-        pattern_len = len(pattern)
-        for i in range(len(pattern)):
-            from_start, from_end = (i, pattern_len-i-1)
-            print(from_start, from_end)
-            if pattern[from_start] == pattern[from_end]:
-                print(from_start, from_end, pattern[from_start])
-        
+        print(sums[-2:])
 
-        # find horizontal mirrors
-        # for i, y in enumerate(pattern):
-        #     print(i, y)
+    print(print(sums)) 
+    return sum(sums)
