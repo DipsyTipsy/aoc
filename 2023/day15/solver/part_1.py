6fa9cb6f4450f0c93d7adcd1882ee67759a0f5f5
Emil Elsetronning <emilandreelsetronning@gmail.com>
2023-12-15 20:40:08 +0100

    Adding solution for day 15

--- 2023/day15/solver/part_1.py
+++ 2023/day15/solver/part_1.py
@@ -1,6 +1,20 @@
 from solver import utils
 
 
+def HASH(step):
+    current_value = 0
+    for chr in step:
+        current_value += ord(chr)
+        current_value = current_value * 17
+        current_value = current_value % 256
+    return current_value
+
 def solve(input_file: str):
-    lines = utils.read_lines(input_file)
-    print(lines)
+    lines = utils.read_lines(input_file)[0].split(",")
+    print()
+
+    sums = []
+    for step in lines:
+        sums.append(HASH(step))
+    
+    return sum(sums)
--- 2023/day15/solver/part_2.py
+++ 2023/day15/solver/part_2.py
@@ -1,5 +1,46 @@
 from solver import utils
+import re
 
+def HASH(step):
+    current_value = 0
+    for chr in step:
+        current_value += ord(chr)
+        current_value = current_value * 17
+        current_value = current_value % 256
+    return current_value
 
 def solve(input_file: str):
-    lines = utils.read_lines(input_file)
+    lines = utils.read_lines(input_file)[0].split(",")
+
+    print(lines)
+    
+    boxes = {x: {} for x in range(256)} 
+    for step in lines:
+        label, operation, lens = re.findall(r"([a-z]+)([\=|\-])(\d+)?", step)[0]
+        box = HASH(label)
+        print(step, label, operation, lens, box)
+
+        boxes[box][label] = lens
+        if(boxes[box][label] == ''):
+            boxes[box].pop(label)
+    
+    sums = []
+    for box_num, box in boxes.items():
+        for i, lens in enumerate(box.values()):
+            power = 1 * (box_num+1) * (i+1) * int(lens)
+            sums.append(power)
+    print(sums)
+
+    return sum(sums)
+
+
+
+        
+
+
+
+
+
+
+
+
