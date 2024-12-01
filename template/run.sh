76e1011c1e7e1579518e1ee656600008ae34ac75
Emil Elsetronning <emilandreelsetronning@gmail.com>
2024-12-01 21:27:19 +0100

    Minor addition to template

--- template/run.sh
+++ template/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-PYTHONDONTWRITEBYTECODE=1 poetry run ptw -c --ignore .venv --ext .txt,.py -- -- -s -raFP -W ignore::pytest.PytestReturnNotNoneWarning -p no:cacheprovider
+PYTHONDONTWRITEBYTECODE=1 uv run ptw -c --ignore .venv --ext .txt,.py -- -s -raFP -W ignore::pytest.PytestReturnNotNoneWarning -p no:cacheprovider
