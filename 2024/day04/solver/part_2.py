549094d5871aeb7557a8227789e16899bdc4a3cc
Emil Elsetronning <emilandreelsetronning@gmail.com>
2024-12-04 08:27:01 +0100

    Adding part 2 solution

--- 2024/day04/solver/part_2.py
+++ 2024/day04/solver/part_2.py
@@ -1,79 +1,28 @@
 from solver import utils
-from collections import defaultdict
 
-
-directions = [
-    (1,0),
-    (1,1),
-    (0,1),
-    (-1,0),
-    (-1,-1),
-    (0,-1),
-    (1, -1),
-    (-1, 1)
-]
-
-
-def search(map, y, x, dir, target, hits):
+def search(map, y, x, dir):
     hit = 0
     new_y = y + dir[0]
     new_x = x + dir[1]
     try:
         if new_y < 0 or new_x < 0:
-            return 0
-        destination = map[new_y][new_x]
+            return ""
+        return map[new_y][new_x]
     except IndexError:
-        return 0
-
-    if target == destination:
-        for hit in hits:
-            hits[y][x] = map[y][x]
-            hits[new_y][new_x] = destination
-            #print("".join(hit))
-
-        hit = 1
-        
-        if hit >= 1:
-            hits[new_y][new_x] = destination
-
-        return hit
-    else:
-        return 0
+        return ""
 
 def solve(input_file: str):
     lines = [[y for y in x] for x in utils.read_lines(input_file)]
-    total_hits = [["." for x in range(len(lines))] for y in range(len(lines))]
-    current_hits = set()
-    for line in lines:
-        print(line)
 
     total = 0 
     for y, line in enumerate(lines):
         for x, char in enumerate(line):
             if char == "A":
-                #print("Found X at",y, x, char)
-                for dir in directions:
-                    hits = [["." for x in range(len(lines))] for y in range(len(lines))]
-                    matches = search(lines, y,x,dir,"M",hits)
+                diags = []
 
-                    if matches:
-                        matches = search(lines, y,x,dir, "S", hits)
-                        hits[y][x] = "A"
-                        occurance = ""
-                        for i, hit in enumerate(hits):
-                            print("".join(hit))
-                            occurance += "".join(hit)
-                            for j, c in enumerate(hit):
-                                if c != ".":
-                                    total_hits[i][j] = c
-                #        print()
+                diags.append(search(lines, y, x, (1,1)) + "A" + search(lines, y, x, (-1,-1)))
+                diags.append(search(lines, y, x, (-1,1)) + "A" + search(lines, y, x, (1,-1)))
 
-                        if occurance not in current_hits:
-                            total += matches
-                            current_hits.add(occurance)
-    
-    print(total)
-    for y, hit in enumerate(total_hits):
-        print("".join(hit))
+                total += sum([all([x in diag for x in ["M", "A", "S"]]) for diag in diags]) == 2
+   
     return total
-
