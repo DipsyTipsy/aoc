2af7670f80781851b3232aa1e0839935c27fd0d9
Emil Elsetrønning <emilandreelsetronning@gmail.com>
2024-12-07 07:40:56 +0100

    Further day6 work

--- 2024/day06/solver/part_2.py
+++ 2024/day06/solver/part_2.py
@@ -1,12 +1,13 @@
 from solver import utils
 from collections import defaultdict
+from copy import deepcopy
 import time
 
-def step(pos, direction, map):
+def step(pos, direction, grid):
     new_y = pos[0] + direction[0]
     new_x = pos[1] + direction[1]
-    if new_y >= 0 and new_x >= 0 and new_y < len(map) and new_x < len(map):
-        return ((new_y, new_x,), map[new_y][new_x])
+    if new_y in range(len(grid)) and new_x in range(len(grid)):
+        return ((new_y, new_x,), grid[new_y][new_x])
     else:
         return (None, None)
 
@@ -20,28 +21,34 @@ def rotate(current_facing):
     elif current_facing == (0, -1):
         return (-1, 0)
 
-def check_loop(start, facing, map):
+def check_loop(start, facing, grid):
     _visited = set()
     _pos = start
     _facing = facing
-    #print("\t- Starting check from", _pos, _steps)
+    print("\t- Starting check from", _pos)
+
+    _step = 0
 
     while True:
         if (_pos, _facing) in _visited:
+        # if this works i go to bed
+        #if _step > 10000:
+            print("\t- Loop", _pos, _facing)
             return True
-
+        
         _visited.add((_pos, _facing))
-        new_pos, item = step(_pos, _facing, map)
+        new_pos, item = step(_pos, _facing, grid)
 
-        if item not in [None, "#"]:
+        if item in [".", "^"]:
         #    print("\t\tWalk")
             _pos = new_pos
         elif item == "#":
         #    print("\t\tTurn")
             _facing = rotate(_facing)
         else:
-            #print("- Got Out, Not Loop", _pos,  _steps)
+            print("\t- Got Out, Not Loop", _pos)
             return False
+        _step += 1
 
 def solve(input_file: str):
     print("PART 2")
@@ -62,34 +69,34 @@ def solve(input_file: str):
 
     while True:
         # Check if possible blocker
-        if str((current_pos, current_facing)) in visited:
-            break
-
         blocker, b_c = step(current_pos, current_facing, lines)
-        if not blocker in possible_blocker and b_c not in  ["#", "^", None]:
-            _lines  = [x.copy() for x in lines.copy()]
+        if not blocker in possible_blocker and b_c == ".":
             print("Check from ", (current_pos, current_facing), "Possible Blocker", blocker, b_c)
-            _lines[blocker[0]][blocker[1]] = "#"
+            lines[blocker[0]][blocker[1]] = "#"
 
-            if check_loop(current_pos, current_facing, _lines):
-                possible_blocker.add(str(blocker))
+            if check_loop(current_pos, current_facing, lines):
+                possible_blocker.add(blocker)
                 block_map[blocker[0]][blocker[1]] = "O"
-                
+            
+            lines[blocker[0]][blocker[1]] = "."
 
-        visited.add(str((current_pos, current_facing)))
+        visited.add((current_pos))
         new_map[current_pos[0]][current_pos[1]] = "X"
 
         new_pos, item = step(current_pos, current_facing, lines)
-        if item not in [None, "#"]:
+        if item in [".", "^"]:
             current_pos = new_pos
         elif item == "#":
             current_facing = rotate(current_facing)
         else:
             new_map[current_pos[0]][current_pos[1]] = "O"
             break
+
+        
         
     print()
     for y in block_map:
         print("".join(y))
     
+    print(len(visited), len(possible_blocker))
     return len(possible_blocker)
