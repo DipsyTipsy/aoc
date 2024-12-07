1b60e8506a5b8ff104770399a02239e3a14d227b
Emil Elsetrønning <emilandreelsetronning@gmail.com>
2024-12-07 12:58:04 +0100

    Rework of solutions, adding solution for part 2

--- 2024/day06/solver/part_1.py
+++ 2024/day06/solver/part_1.py
@@ -1,79 +1,66 @@
+
 from solver import utils
-import sys
-import time
 
-def move(pos, direction, map):
-    new_y = pos[0] + direction[0]
-    new_x = pos[1] + direction[1]
-    if not (new_y >= 0 and new_x >= 0):
+rotate = {
+    (-1,  0): (0, 1),
+    ( 0,  1): (1, 0),
+    ( 1,  0): (0, -1),
+    ( 0, -1): (-1, 0),
+}
+
+def get_pos(pos, grid):
+    if all([cord in range(len(grid)) for cord in pos]):
+        return grid[pos[0]][pos[1]]
+    else:
         return None
 
-    try:
-        if map[new_y][new_x] == "#":
-            return pos
-        else:
-            return (new_y, new_x)
-    except:
-        return None
+def step(pos, facing, grid):
+    n_pos = (pos[0]+facing[0], pos[1] + facing[1])
 
+    val = get_pos(n_pos, grid)
+    if val:
+        return n_pos, val
+    else:
+        return None, None
 
+def move(pos, facing, grid):
+    next_pos, next_val = step(pos, facing, grid)
+    next_facing = facing
+    match next_val:
+        case ".":
+            current_pos = next_pos
+            next_facing = facing
+        case "#":
+            next_pos = pos
+            next_facing = rotate[facing]
+            print("Collision")
+        case None:
+            next_pos = None
+            next_facing = None
+            print("OUT")
+
+    return next_pos, next_facing
 
 def solve(input_file: str):
+    print("PART 2")
+    print()
     lines = [[y for y in x] for x in utils.read_lines(input_file)]
 
-    obstacles = set()
-    guard = {}
-    visited = set()
     for y, line in enumerate(lines):
         for x, char in enumerate(line):
-            if char == "#":
-                obstacles.add((y,x))
-            if not char in [".", "#"]:
-                guard["pos"] = (y,x)
-                guard["facing"] = (-1, 0)
-    
-    current_pos = guard["pos"]
-    current_facing = guard["facing"]
-
-    steps = 0
-    new_map  = lines.copy()
-    got_out = False
-    while not got_out and steps < 10000:
-        visited.add(current_pos)
-        new_map[current_pos[0]][current_pos[1]] = "X"
-
-        new_pos = move(current_pos, current_facing, lines)
-
-        if new_pos != current_pos and new_pos:
-            #print(f"{current_pos}, Next valid, moving: {new_pos} DIR {current_facing}")
-            current_pos = new_pos
-
-        elif new_pos == current_pos:
-            if current_facing == (-1,0):
-                current_facing = (0, 1)
-            elif current_facing == (0, 1):
-                current_facing = (1, 0)
-            elif current_facing == (1, 0):
-                current_facing = (0, -1)
-            elif current_facing == (0, -1):
+            if char == "^":
+                current_pos = (y,x)
                 current_facing = (-1, 0)
-        else:
-            got_out = True
-            new_map[current_pos[0]][current_pos[1]] = "O"
-            print("HE GOT OUT")
-            print(current_pos, new_pos, current_facing)
-        
-        #print("\033[u", end="")
-        #for y in new_map:
-        #    print("".join(y))
-        #time.sleep(0.001)
-        
-    #print(len(visited))
+    
+    print("Starting from", current_pos, current_facing)
 
-    #for y in new_map:
-    #        print("".join(y))
+    visited = set()
+    while current_pos:
+        if current_pos:
+            visited.add(current_pos)
+            print(current_pos, current_facing)
+        current_pos, current_facing = move(current_pos, current_facing, lines)
+
+
+    print(len(visited))
     return len(visited)
-        
-
-
-
--- 2024/day06/solver/part_2.py
+++ 2024/day06/solver/part_2.py
@@ -1,49 +1,43 @@
+
 from solver import utils
 
-def step(pos, direction, grid):
-    new_y = pos[0] + direction[0]
-    new_x = pos[1] + direction[1]
-    if new_y in range(len(grid)) and new_x in range(len(grid)):
-        return ((new_y, new_x,), grid[new_y][new_x])
+rotate = {
+    (-1,  0): (0, 1),
+    ( 0,  1): (1, 0),
+    ( 1,  0): (0, -1),
+    ( 0, -1): (-1, 0),
+}
+
+def get_pos(pos, grid):
+    if all([cord in range(len(grid)) for cord in pos]):
+        return grid[pos[0]][pos[1]]
     else:
-        return (None, None)
+        return None
 
-def rotate(current_facing):
-    if current_facing == (-1,0):
-        return (0, 1)
-    elif current_facing == (0, 1):
-        return (1, 0)
-    elif current_facing == (1, 0):
-        return (0, -1)
-    elif current_facing == (0, -1):
-        return (-1, 0)
+def step(pos, facing, grid):
+    n_pos = (pos[0]+facing[0], pos[1] + facing[1])
 
-def check_loop(start, facing, grid):
-    _visited = set()
-    _pos = start
-    _facing = facing
-    print("\t- Starting check from", _pos)
+    val = get_pos(n_pos, grid)
+    if val:
+        return n_pos, val
+    else:
+        return None, None
 
-    _step = 0
+def move(pos, facing, grid):
+    next_pos, next_val = step(pos, facing, grid)
+    next_facing = facing
+    match next_val:
+        case ".":
+            current_pos = next_pos
+            next_facing = facing
+        case "#":
+            next_pos = pos
+            next_facing = rotate[facing]
+        case None:
+            next_pos = None
+            next_facing = None
 
-    while True:
-        if (_pos, _facing) in _visited:
-            print("\t- Loop", _pos, _facing)
-            return True
-        
-        _visited.add((_pos, _facing))
-        new_pos, item = step(_pos, _facing, grid)
-
-        if item in [".", "^"]:
-        #    print("\t\tWalk")
-            _pos = new_pos
-        elif item == "#":
-        #    print("\t\tTurn")
-            _facing = rotate(_facing)
-        else:
-            print("\t- Got Out, Not Loop", _pos)
-            return False
-        _step += 1
+    return next_pos, next_facing
 
 def solve(input_file: str):
     print("PART 2")
@@ -52,46 +46,43 @@ def solve(input_file: str):
 
     for y, line in enumerate(lines):
         for x, char in enumerate(line):
-            if char not in [".", "#"]:
+            if char == "^":
                 current_pos = (y,x)
                 current_facing = (-1, 0)
     
-    print("Starting from", current_pos)
-    new_map  = [x.copy() for x in lines.copy()]
-    block_map = [x.copy() for x in lines.copy()]
-    possible_blocker = set()
+    start_pos = current_pos
+    start_facing = current_facing
+    print("Starting from", current_pos, current_facing)
+
     visited = set()
+    blockers = set()
+    while current_pos:
+        if current_pos:
+            visited.add(current_pos)
+            #print(current_pos, current_facing)
 
-    while True:
-        # Check if possible blocker
-        blocker, b_c = step(current_pos, current_facing, lines)
-        if blocker not in possible_blocker and b_c == ".":
-            print("Check from ", (current_pos, current_facing), "Possible Blocker", blocker, b_c)
-            lines[blocker[0]][blocker[1]] = "#"
-
-            if check_loop(current_pos, current_facing, lines):
-                possible_blocker.add(blocker)
-                block_map[blocker[0]][blocker[1]] = "O"
-            
-            lines[blocker[0]][blocker[1]] = "."
-
-        visited.add((current_pos))
-        new_map[current_pos[0]][current_pos[1]] = "X"
-
-        new_pos, item = step(current_pos, current_facing, lines)
-        if item in [".", "^"]:
-            current_pos = new_pos
-        elif item == "#":
-            current_facing = rotate(current_facing)
-        else:
-            new_map[current_pos[0]][current_pos[1]] = "O"
-            break
-
-        
-        
-    print()
-    for y in block_map:
-        print("".join(y))
+        current_pos, current_facing = move(current_pos, current_facing, lines)
     
-    print(len(visited), len(possible_blocker))
-    return len(possible_blocker)
+    for pos in visited:
+        if pos not in blockers:
+            lines[pos[0]][pos[1]] = "#"
+            print("- Checking blocker", pos)
+            simulated_pos, simulated_facing = start_pos, start_facing
+
+            simulated_visited = set()
+            while simulated_pos and not (simulated_pos, simulated_facing) in simulated_visited:
+
+                if simulated_pos:
+                    simulated_visited.add((simulated_pos, simulated_facing))
+
+                simulated_pos, simulated_facing = move(simulated_pos, simulated_facing, lines)
+
+            if (simulated_pos, simulated_facing) in simulated_visited and simulated_pos:
+                blockers.add(pos)
+
+            lines[pos[0]][pos[1]] = "."
+
+
+    print(len(blockers))
+    print(len(visited))
+    return len(blockers)
