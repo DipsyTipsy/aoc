4e9afd47f4e6cbacbc2af2e3c171bcefebb40e10
DipsyTipsy <emilandreelsetronning@gmail.com>
2025-12-02 11:59:51 +0100

    Optimizing a bit

--- 2025/day02/solver/part_1.py
+++ 2025/day02/solver/part_1.py
@@ -2,25 +2,21 @@ from solver import utils
 
 def check_valid(num):
     length = len(str(num))
-    if len(str(num)) % 2 == 0:
-        a = str(num)[:int(length/2)]
-        b = str(num)[int(length/2):]
+    half = int(length/2)
+    if not length % 2:
+        a = str(num)[:half]
+        b = str(num)[half:]
         return a  == b
-
     return False
 
 def solve(input_file: str):
     lines = utils.read_lines(input_file)
     ranges = [(int(x.split("-")[0]), int(x.split("-")[1])) for x in lines[0].split(",")]
-    invalid = []
+    invalid = 0
 
     for min,max in ranges:
-
         for i in range(min, max+1):
             if check_valid(i):
-                # print("invalid", i)
-                invalid.append(i)
+                invalid += i
     
-    return sum(invalid)
-
-
+    return invalid
--- 2025/day02/solver/part_2.py
+++ 2025/day02/solver/part_2.py
@@ -1,32 +1,23 @@
 from solver import utils
-import re
 
 def check_valid(num):
     num = str(num)
-    for check in range(int(len(num)/2)+1):
-        # r = re.sub(num[:check], "", num)
-        r = num.replace(num[:check], "")
-        if len(r) == 0:
-            return True
-
-
+    for check in range(1,int(len(num)/2)+1):
+        if not len(num) % check:
+            to_check = num[:check]
+            r = num.replace(to_check, "")
+            if len(r) == 0:
+                return True
     return False
 
 def solve(input_file: str):
     lines = utils.read_lines(input_file)
     ranges = [(int(x.split("-")[0]), int(x.split("-")[1])) for x in lines[0].split(",")]
-    invalid = []
+    invalid = 0
 
     for min,max in ranges:
-        print(min,max)
-
         for i in range(min, max+1):
             if check_valid(i):
-                print("invalid", i)
-                invalid.append(i)
+                invalid += i
     
-    return sum(invalid)
-
-
-
-
+    return invalid
