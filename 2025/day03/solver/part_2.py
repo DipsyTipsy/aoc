95c00f380acd32f77046770f4d062037ab41f723
DipsyTipsy <emilandreelsetronning@gmail.com>
2025-12-03 09:21:09 +0100

    Solving part 2

--- 2025/day03/solver/part_2.py
+++ 2025/day03/solver/part_2.py
@@ -13,22 +13,15 @@ def solve(input_file: str):
         result = ""
         p_idx = 0
         min_idx = 0
-        max_idx = len(line)-12
-        print(line)
-        cur_line = line[:-12]
-        for i in range(12):
+        max_idx = len(line)-11
+        cur_line = line[min_idx:max_idx]
+        for i in range(1,13):
             p_idx, a = find_max(cur_line)
-            print(cur_line, a, p_idx, min_idx, max_idx)
             result += str(a)
-            if i == 10:
-                cur_line = line[min_idx:]
-            else:
-                min_idx = min_idx + p_idx + 1
-                max_idx = len(line)-12+(i+2)
-                cur_line = line[min_idx:max_idx]
+
+            min_idx = min_idx + p_idx + 1
+            max_idx = len(line)-12+(i+1)
+            cur_line = line[min_idx:max_idx]
 
         total += int(result)
-        print(result)
-        print()
     return total
-        
